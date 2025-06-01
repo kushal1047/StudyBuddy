eb65d36c2c5327294db67c90e2cbdbd650ffc6a2
Kushal Niraula <kushal463334@gmail.com>
2025-06-02 02:04:00 +1000

    configure Toast component in App root

--- StudyBuddyMobile/App.tsx
+++ StudyBuddyMobile/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import Toast from "react-native-toast-message";
 import { AuthProvider } from "./src/contexts/AuthContext";
 import RootNavigator from "./src/navigation/RootNavigator";
 
 export default function App() {
   return (
-    <AuthProvider>
-      <RootNavigator />
-    </AuthProvider>
+    <>
+      <AuthProvider>
+        <RootNavigator />
+      </AuthProvider>
+      <Toast />
+    </>
   );
 }
